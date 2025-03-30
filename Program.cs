a92fd6c1b34d79a185edb0c6494ffac84c6c1fe3
Gabriel <gabrielmito054@gmail.com>
2025-03-30 14:41:05 -0300

    Adicionado toda a estrutura do Program. Projeto finalizado

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Pagamentos
 {
@@ -6,7 +7,26 @@ namespace Pagamentos
     {
         static void Main(string[] args)
         {
+            System.Console.WriteLine("Entre com os dados do contrato:");
+
+            System.Console.Write("Número: ");
+            int num = int.Parse(Console.ReadLine());
+            System.Console.Write("Data (dd/MM/yyyy): ");
+            DateTime data = DateTime.ParseExact(Console.ReadLine(),"dd/MM/yyyy",CultureInfo.InvariantCulture);
+            System.Console.Write("Valor do contrato: ");
+            double valor = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            System.Console.Write("Entre com o número de parcelas: ");
+            int parcelas = int.Parse(Console.ReadLine());
+            System.Console.WriteLine();
+
+            Contrato contrato = new Contrato(num,data,valor);
+            ContratoDeServico contratoDeServico = new ContratoDeServico(new ServicoPaypal());
+            contratoDeServico.ProcessarContrato(contrato,parcelas);
             
+            foreach(var item in contrato.parcelas)
+            {
+                System.Console.WriteLine(item);
+            }
         }
     }
 }
